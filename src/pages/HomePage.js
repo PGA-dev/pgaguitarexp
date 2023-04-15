0e3e72d57d1cc79bab297d0d51a937ae216c7b5f
PGA-dev <paulgadam@gmail.com>
2023-04-15 16:56:11 -0600

    updated homepage removed front carousel comments

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,5 @@
 import { Container } from 'reactstrap';
-//import FrontEndDisplay from '../features/FrontDisplay/FrontEndDisplay';
 import SubHeader from '../site/SubHeader';
-//import FrontEndDisplayCarousel from '../features/FrontDisplay/FrontEndDisplayUC'
 import FrontDisplayCarousel from '../features/FrontDisplay/FrontEndDisplayCarousel';
 
 
